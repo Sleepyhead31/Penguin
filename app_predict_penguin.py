ba81abc852490179074cb54743bec9b45ee249b9
Sleepyhead31 <103825996+Sleepyhead31@users.noreply.github.com>
2023-04-09 15:19:22 +0700

    Add files via upload

--- app_predict_penguin.py
+++ app_predict_penguin.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 import pickle
@@ -12,32 +11,33 @@ tab1, tab2, tab3 = st.tabs(["Penquin Prediction", "Evaluation"])
 
 with tab1:
 
-        model = pickle.load(open('model.penguins.sav','rb'))
-        species_encoder = pickle.load(open('encoder.species.sav','rb'))
-        island_encoder = pickle.load(open('encoder.island.sav','rb'))
-        sex_encoder = pickle.load(open('encoder.sex.sav','rb'))
-        evaluations = pickle.load(open('evals.all.sav','rb'))
+    model = pickle.load(open('model.penguins.sav','rb'))
+    species_encoder = pickle.load(open('encoder.species.sav','rb'))
+    island_encoder = pickle.load(open('encoder.island.sav','rb'))
+    sex_encoder = pickle.load(open('encoder.sex.sav','rb'))
+    evaluations = pickle.load(open('evals.all.sav','rb'))
 
-        st.title('Penguin Species Prection :) ')
+    st.title('Penguin Species Prection :) ')
 
-        x1 = st.radio('Select island', island_encoder.classes_)
-        x1 = island_encoder.transform([x1])[0]
-        # x1 
-        x2 = st.slider('Select culmen length (mm)', 25, 70, 40)
-        x3 = st.slider("เลือก culmen depth (mm)", 10,30,15 )
-        x4 = st.slider("เลือก flipper length (mm)", 150,250,200)
-        x5 = st.slider("เลือก body mass (g)", 2500,6500,3000)
-        x6 = st.radio("เลือก sex ",sex_encoder.classes_)
-        x6 = sex_encoder.transform([x6])[0]
+    x1 = st.radio('Select island', island_encoder.classes_)
+    x1 = island_encoder.transform([x1])[0]
 
-        x_new = pd.DataFrame(data=np.array([x1, x2, x3, x4, x5, x6]).reshape(1,-1), 
+    x2 = st.slider('Select culmen length (mm)', 25, 70, 40)
+    x3 = st.slider("เลือก culmen depth (mm)", 10,30,15 )
+    x4 = st.slider("เลือก flipper length (mm)", 150,250,200)
+    x5 = st.slider("เลือก body mass (g)", 2500,6500,3000)
+    x6 = st.radio("เลือก sex ",sex_encoder.classes_)
+    x6 = sex_encoder.transform([x6])[0]
+
+    x_new = pd.DataFrame(data=np.array([x1, x2, x3, x4, x5, x6]).reshape(1,-1), 
                      columns=['island', 'culmen_length_mm', 'culmen_depth_mm','flipper_length_mm', 'body_mass_g', 'sex'])
 
-        pred = model.predict(x_new)
+    pred = model.predict(x_new)
 
-        st.write('Predicted Species: ' , species_encoder.inverse_transform(pred)[0])
+    st.write('Predicted Species: ' , species_encoder.inverse_transform(pred)[0])
 
 with tab2:
+
     st.header("Evaluations on Five Techniques")
     evaluations = pickle.load(open('evals.all.sav','rb'))
     
